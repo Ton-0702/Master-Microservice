86704c0b5a85ca21f019dc2daedb3b2df72bb891
DESKTOP-T6M3KKV\pc <19447201.iuh@gmail.com>
2024-11-29 23:11:41 +0700

    Update Cards Microservice

--- section2/cards/src/main/java/com/eazybytes/cards/service/ICardsService.java
+++ section2/cards/src/main/java/com/eazybytes/cards/service/ICardsService.java
@@ -21,4 +21,10 @@ public interface ICardsService {
      * @return boolean indicating if the update of card details is successful or not
      */
     boolean updateCard(CardsDto cardsDto);
+
+    /**
+     * @param mobileNumber - Input Mobile Number
+     * @return boolean indicating if the delete of card details is successful or not
+     */
+    boolean deleteCard(String mobileNumber);
 }
