10c180b678258d2e40ad297ffe938c0e9d7856de
Toan <anhtoan@posco.net>
2024-12-11 15:28:19 +0700

    Update section 6 Configuration Microservice

---
+++ section6/README.md
@@ -0,0 +1,4 @@
+# Configuration Microservice
+
+**Version 1:**
+1. Change all Google Jib in Microservice to build Docker Image 
---
+++ section6/v1-springboot/cards/src/main/java/com/eazybytes/cards/entity/Cards.java
@@ -0,0 +1,36 @@
+package com.eazybytes.cards.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@ToString // convert entity object to String format
+@NoArgsConstructor
+@AllArgsConstructor
+public class Cards extends BaseEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "card_id")
+    private Long cardId;
+
+    @Column(name = "mobile_number")
+    private String mobileNumber;
+
+    @Column(name = "card_number")
+    private String cardNumber;
+
+    @Column(name = "card_type")
+    private String cardType;
+
+    @Column(name = "total_limit")
+    private int totalLimit;
+
+    @Column(name = "amount_used")
+    private int amountUsed;
+
+    @Column(name = "available_amount")
+    private int availableAmount;
+}
